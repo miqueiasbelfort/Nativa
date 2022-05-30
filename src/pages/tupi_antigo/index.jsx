2301eabb4dab3504ca3d07ec692740858f2d780b
miqueiasbelfort <kawasousa1554@gmail.com>
2022-05-30 19:47:31 -0300

    components styles

--- src/pages/tupi_antigo/index.jsx
+++ src/pages/tupi_antigo/index.jsx
@@ -1,24 +1,24 @@
-import {Link} from 'react-router-dom'
+import styles from "./Tupi_antigo.module.css"
 import {GiSnakeEgg} from "react-icons/gi"
 
 import Circle from '../../components/Circle'
 
 const Tupi_antigo = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>TUPI ANTIGO</h1>
       <div>
 
-        <div className='simple_container all_line'>
+        <div className={styles.simple_container}>
           <h2>Introdução</h2>
           <Circle
             link="/tupi_antigo/introducao"
             icon={<GiSnakeEgg/>}
           />
         </div>
-        <div className='duo_container'>
+        <div className={styles.duo_container}>
 
-          <div className='all_line'>
+          <div>
             <h2>Introdução</h2>
             <Circle
               link="/tupi_antigo/introducao"
@@ -26,7 +26,7 @@ const Tupi_antigo = () => {
             />
           </div>
 
-          <div className='all_line'>
+          <div>
             <h2>Introdução</h2>
             <Circle
               link="/tupi_antigo/introducao"
