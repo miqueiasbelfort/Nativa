b4a99cd88e97d343de14ebbb0d462822f6aae947
miqueiasbelfort <kawasousa1554@gmail.com>
2022-05-30 21:49:12 -0300

    home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ import Tupi_Antigo from "../../assets/tupi_antigo.jpg"
 const Home = () => {
   return (
     <div className={styles.container}>
-        <h1>Aprenda linguas nativas Brasileiras</h1>
+        <h1>Aprenda línguas nativas Brasileiras</h1>
         <Link className={styles.about} to="/about">Saber mais...</Link>
         <div className={styles.cards_container}>
             {
